ef5a9d6e733334d1a9cd9e44f1f6692933204ec5
LAPTOP-97H7B5O5\manel <manel.sayadi@yahoo.fr>
2018-02-12 17:06:32 +0100

    premier test

---
+++ src/test/java/com/ipiecoles/java/java340/model/CommercialTest.java
@@ -0,0 +1,18 @@
+package com.ipiecoles.java.java340.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class CommercialTest {
+
+@Test
+public void testGetPrimeAnnuelleWithCANull(){
+    //Given
+    Commercial commercial = new Commercial();
+    commercial.setCaAnnuel(null);
+    //When
+    Double primeAnnuelle = commercial.getPrimeAnnuelle();
+    //then
+    Assertions.assertThat(primeAnnuelle).isEqualTo(500D);
+
+}
