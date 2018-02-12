fe9e71d340b3ab41cde6b8104d28ac3a2b93a389
PEREAU <alban.pereau@cgi.com>
2018-02-12 15:28:31 +0100

    modif travis.yml et application

--- src/main/java/com/ipiecoles/java/java340/SpringWebApplication.java
+++ src/main/java/com/ipiecoles/java/java340/SpringWebApplication.java
@@ -7,12 +7,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringWebApplication extends SpringBootServletInitializer {
+public class SpringWebApplication  {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringWebApplication.class);
-    }
+    
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringWebApplication.class, args);
