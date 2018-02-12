1cbea39a4ed72e92f96b9661fffa60064d529002
shin_taku@hotmail.com <shin_taku@hotmail.com>
2018-02-12 16:40:20 +0100

    TU CommercialTest, two methods tested

---
+++ src/test/java/com/ipiecoles/java/java340/model/CommercialTest.java
@@ -0,0 +1,31 @@
+package com.ipiecoles.java.java340.model;
+
+import com.ipiecoles.java.java340.model.Commercial;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class CommercialTest {
+
+    @Test
+    public void testPerformanceEquality() {
+        //Given
+        Integer perf = new Integer(3);
+        Commercial commercial = new Commercial();
+        commercial.setPerformance(2);
+        //When
+        Boolean test = commercial.performanceEgale(3);
+        //Then
+        Assertions.assertThat(test).isFalse();
+    }
+
+    @Test
+    public void testGetPrimeAnnuelleWithCANull(){
+        //Given
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(null);
+        //When
+        Double prime = commercial.getPrimeAnnuelle();
+        //Then
+        Assertions.assertThat(prime).isEqualTo(500d);
+    }
+}
