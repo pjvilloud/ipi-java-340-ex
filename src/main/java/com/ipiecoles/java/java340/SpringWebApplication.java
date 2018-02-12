48f0ad634937d57bd861b4db4448ccd4b41a78b2
Julien <juju.om26@gmail.com>
2018-02-12 15:19:32 +0100

    .travis.yml

--- src/main/java/com/ipiecoles/java/java340/SpringWebApplication.java
+++ src/main/java/com/ipiecoles/java/java340/SpringWebApplication.java
@@ -1,21 +1,12 @@
 package com.ipiecoles.java.java340;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringWebApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringWebApplication.class);
-    }
+public class SpringWebApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringWebApplication.class, args);
     }
-
 }
