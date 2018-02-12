43c17b25fb8b9400377fbf8d3fbd0d13bf50dd06
Pcharlet <charletpierre6@gmail.com>
2018-02-12 16:39:19 +0100

    ajout commercial test

---
+++ src/test/java/com/ipiecoles/java/java340/model/CommercialTest.java
@@ -0,0 +1,14 @@
+package com.ipiecoles.java.java340.model;
+
+public class CommercialTest {
+
+	public Boolean TestGetPrimeAnnuel() {
+		boolean bool =false;
+		
+		
+		return bool;
+		
+		
+	}
+	
+}
