4c35a2bb31d707b50e480a48f4b8752b15460787
remifoyard <rfpro8@gmail.com>
2018-02-12 16:37:52 +0100

    test getprime

---
+++ src/test/java/com/ipiecoles/java/java340/model/CommercialTest.java
@@ -0,0 +1,20 @@
+package com.ipiecoles.java.java340.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class CommercialTest
+{
+    @Test
+    public void testGetPrimeAnnuelleWithCaNull(){
+        //Given
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(null);
+
+        //When
+        Double prime = commercial.getPrimeAnnuelle();
+
+        //Then
+        Assertions.assertThat(prime).isEqualTo(500d);
+    }
+}
