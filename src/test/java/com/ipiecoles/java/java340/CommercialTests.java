e7e1195172a70c97b26d5d8cbaa6b85051a4564d
salyND <salynd@gmail.com>
2018-02-12 16:39:00 +0100

    Ajout CommercialTest
    Ajout CommercialTest

---
+++ src/test/java/com/ipiecoles/java/java340/CommercialTests.java
@@ -0,0 +1,22 @@
+package com.ipiecoles.java.java340;
+
+import org.junit.Test;
+
+public class CommercialTests {
+
+	/*Methode prime annuelle
+	public Double getPrimeAnnuelle() {
+		if(this.getCaAnnuel() != null){
+			return Math.max(Math.ceil(this.getCaAnnuel() * 0.05), 500d);
+		}
+		else {
+			return 500d;
+		}
+	}*/
+	
+	 @Test //dans org.junit.Test
+	  public void testPrimeAnnuelle(){
+		 
+		 
+	 }
+}
