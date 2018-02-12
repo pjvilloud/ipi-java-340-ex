584562b032b47480140f2d521332a64a308efdc8
Yannouxt <laforest.yann@gmail.com>
2018-02-12 16:57:08 +0100

    Add more tests to CommercialTest

--- src/test/java/test/ipiecoles/java/java340/CommercialTest.java
+++ src/test/java/test/ipiecoles/java/java340/CommercialTest.java
@@ -20,16 +20,55 @@ public class CommercialTest {
 	}
 	
 	@Test
-	public void testGetPrimeAnnuelleWithCASup0Inf500() {
+	public void testGetPrimeAnnuelleWithCASup10000() {
 		//Given
 		Commercial com = new Commercial();
-		com.setCaAnnuel(499d);
+		com.setCaAnnuel(10001d);
 		
 		//When
 		Double prime = com.getPrimeAnnuelle();
 		
 		//Then
-		Assertions.assertThat(prime).isEqualTo(500d);;
+		Assertions.assertThat(prime).isNotEqualTo(500d);
+	}
+	
+	@Test
+	public void testGetPrimeAnnuelleWithCAInf0() {
+		//Given
+		Commercial com = new Commercial();
+		com.setCaAnnuel(-100d);
+		
+		//When
+		Double prime = com.getPrimeAnnuelle();
+		
+		//Then
+		Assertions.assertThat(prime).isEqualTo(500d);
+	}
+	
+	@Test
+	public void testGetPrimeAnnuelleWithCABetween0And10000() {
+		//Given
+		Commercial com = new Commercial();
+		com.setCaAnnuel(100d);
+		
+		//When
+		Double prime = com.getPrimeAnnuelle();
+		
+		//Then
+		Assertions.assertThat(prime).isEqualTo(500d);
+	}
+	
+	@Test
+	public void testGetPrimeAnnuelleWithCATooBig() {
+		//Given
+		Commercial com = new Commercial();
+		com.setCaAnnuel(Double.MAX_VALUE*2);
+		
+		//When
+		Double prime = com.getPrimeAnnuelle();
+		
+		//Then
+		Assertions.assertThat(prime).isEqualTo(Double.MAX_VALUE*2*0.05);
 	}
 	
 
