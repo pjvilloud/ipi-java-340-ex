b418237645f445fae71fece1bf9602537115d7db
Abderzak SLIMANI <abderzak.slimani@gmail.com>
2018-02-12 16:41:57 +0100

    Modification de CommercialTest

--- src/test/java/com/ipiecoles/java340/model/CommercialTest.java
+++ src/test/java/com/ipiecoles/java340/model/CommercialTest.java
@@ -1,5 +1,20 @@
 package com.ipiecoles.java340.model;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import com.ipiecoles.java.java340.model.Commercial;
 
 public class CommercialTest {
-
+	
+	@Test
+	public void testGetPrimeAnnuelleWithCANull() {
+		//Given
+		Commercial commercial = new Commercial();
+		commercial.setCaAnnuel(null);
+		
+		//When
+		Double prime = commercial.getPrimeAnnuelle();
+		
+		//Then
+		Assertions.assertThat(prime).isEqualTo(500d);
+	}
 }
