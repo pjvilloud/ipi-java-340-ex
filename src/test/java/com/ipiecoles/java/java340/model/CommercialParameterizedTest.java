d7c5613c282f81374fc5b443cf7a38a7e797bc50
Michael Chartoire <chartoire.michael@neuf.fr>
2018-02-13 10:17:05 +0100

    test 3

--- src/test/java/com/ipiecoles/java/java340/model/CommercialParameterizedTest.java
+++ src/test/java/com/ipiecoles/java/java340/model/CommercialParameterizedTest.java
@@ -10,49 +10,37 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
-public class CommercialParameterizedTest
-{
-	
-	/*@RunWith(value = Parameterized.class)//org.junit.runners.Parameterized
-	public class CommercialTest
-	{
-	  @Parameter(value = 0)//org.junit.runners.Parameterized.Parameter
-	  public Double primeAnnuelle;
-	  @Parameter(value = 1)
-	  public Boolean calculePrimeOK;
-	  //org.junit.runners.Parameterized.Parameters
-	  @Parameters(name = "primeAnnuelle {0} est valide : {1}")
-	  public Collection<Object[]> data()
-	  {
-	      return Arrays.asList(new Object[][]
-	    		  {
-	        {"0d", false}, {"500", true}, {"null", false}
-	      });
-	  }
-	  
-	  @Test
-	  public void testCheckPrime()
-	  {
-	  	//Given, when, Then
-	  	 * Assertions.assertThat(Commercial.checkPrime(primeAnnuelle).isEqualTo(calculePrimeOK);
-	  }
-	}*/
+
+
+	 
 	
 	@RunWith(value = Parameterized.class)//org.junit.runners.Parameterized
-	public class CommercialTest
+	public class CommercialParameterizedTest
 	{
 	  @Parameter(value = 0)//org.junit.runners.Parameterized.Parameter
 	  public Integer performance;
 	  @Parameter(value = 1)
 	  public Note quelleNote;
 	  //org.junit.runners.Parameterized.Parameters
-	  @Parameters(name = "performance {0} est valide : {1}")
-	  public Collection<Object[]> data()
+	  @Parameterized.Parameters(name = "performance {0} est valide : {1}")
+	  public static Collection<Object[]> data()
 	  {
 	      return Arrays.asList(new Object[][]
 	    		  {
-	        {null, "Absent"}, {50, Note.INSUFFISANT}, {100, Note.PASSABLE}, {150, Note.BIEN}, {200, Note.TRES_BIEN}
+	        {null, "Absent"}, {50, Note.INSUFFISANT}, {100, Note.PASSABLE}, {150, Note.BIEN}, {200, Note.TRES_BIEN}, {600, null}
 	      });
 	  }
+	
+		public void testEquivalenceNote( )
+		{
+			//Given
+			Commercial commercial = new Commercial();
+			commercial.setPerformance(performance);
+			//When
+			Note note = commercial.equivalenceNote();
+			//Then
+			Assertions.assertThat(note).isEqualTo(quelleNote);
+		}
 	}
+	
 }
