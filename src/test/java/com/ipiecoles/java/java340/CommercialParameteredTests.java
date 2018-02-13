801dfb20d2979de5d263e52aaae9128ec4014b5e
salyND <salynd@gmail.com>
2018-02-13 10:27:18 +0100

    tests parametrés et tests unitaires

---
+++ src/test/java/com/ipiecoles/java/java340/CommercialParameteredTests.java
@@ -0,0 +1,58 @@
+package com.ipiecoles.java.java340;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.ipiecoles.java.java340.model.Commercial;
+import com.ipiecoles.java.java340.model.Note;
+
+@RunWith(value = Parameterized.class)
+public class CommercialParameteredTests {
+
+		/* L'annotation parameter permet d'identifier le numero de parametre. 
+		 * Quand on a plusieurs couples de données a tester on numerote chaque parametre utilisé 
+		 * Les numéros commencent tjrs par 0
+		 */
+		/*Il existe une librairie Junit qui permet de faciliter l'écriture des tests pour un JUNIT supérieur à 4.6. Il
+		 * faut ajouter la dépendance dans le pom.xml : cf Git JUNITParams de Pragmatists */
+		//Test de l'Equivalence Note
+		@Parameter(value = 0) 
+		public Integer performance; 
+	
+		@Parameter(value = 1) 
+		public Note result;
+		
+		@Parameters (name = "performance {0} est équivalent à : {1}")
+		public static Collection<Object[]> data () {
+			return Arrays.asList(new Object[][]{
+				{0, Note.INSUFFISANT}, 
+				{50,Note.INSUFFISANT},
+				{100, Note.PASSABLE},
+				{150, Note.BIEN},
+				{200, Note.TRES_BIEN},
+				{null,null},
+				{600, null}
+			});
+		}
+		
+		@Test
+		public void testEquivalenceNote(){
+			//Given
+			Commercial commercial = new Commercial();
+			commercial.setPerformance(performance);
+			
+			//When
+			Note Notefinale = commercial.equivalenceNote();
+			
+			//Then
+			Assertions.assertThat(Notefinale).isEqualTo(result);
+		}
+		
+}
--- src/test/java/com/ipiecoles/java/java340/CommercialTests.java
+++ src/test/java/com/ipiecoles/java/java340/CommercialTests.java
@@ -5,6 +5,13 @@ import org.junit.Test;
 import com.ipiecoles.java.java340.model.Commercial;
 
 
+/*Un test doit etre unitaire. Pas necessaire de tester tous les cas possibles 
+ * mais le plus important est de verifier au moins les cas aux limites. 
+ * On peut afficher le pourcentage de couverture de la classe par les tests mais avec une version 
+ * plus récente d'eclipse en faisant rus As Coverage
+
+*/
+
 public class CommercialTests {
 
 	 @Test //dans org.junit.Test
@@ -21,14 +28,56 @@ public class CommercialTests {
 		Assertions.assertThat(prime).isEqualTo(500d);
 	 }
 	 
+	 
+	 @Test //dans org.junit.Test
+	  public void testPrimeAnnuelleCa0(){
+		 
+		 //Given = 
+		 Commercial commercial = new Commercial();
+		 commercial.setCaAnnuel(0d);
+		 
+		 //When
+		Double prime = commercial.getPrimeAnnuelle();
+		 		
+		//Then
+		Assertions.assertThat(prime).isEqualTo(500d);
+	 }
+	 
+	 @Test //dans org.junit.Test
+	  public void testPrimeAnnuelleWithCA9000(){
+		 
+		 //Given = 
+		 Commercial commercial = new Commercial();
+		 commercial.setCaAnnuel(9000d);
+		 
+		 //When
+		Double prime = commercial.getPrimeAnnuelle();
+		 		
+		//Then
+		Assertions.assertThat(prime).isEqualTo(500d);
+	 }
+	 
 	 @Test //dans org.junit.Test
 	  public void testPerformance(){
 		 Commercial commercial = new Commercial();
+		 commercial.setPerformance(1);
 		  
 		 //Given
 		 Integer performance = commercial.getPerformance();
 		 
 		 //Then
-		 Assertions.assertThat(performance).isNull();
+		 Assertions.assertThat(performance).isNotNull();
 	 }
+	 
+	 @Test //dans org.junit.Test
+	  public void testNom(){
+		 Commercial commercial = new Commercial();
+		
+		 //Given 
+		 String nom = commercial.getNom();
+		 
+		 //then
+		 Assertions.assertThat(nom).isNotBlank();
+	 }
+	 
 }
