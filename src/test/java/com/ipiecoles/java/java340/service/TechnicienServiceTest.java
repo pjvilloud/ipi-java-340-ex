7e6d6707bb83b7e52269deb165327c7b5726969e
remifoyard <rfpro8@gmail.com>
2018-02-13 16:39:31 +0100

    16h39

--- src/test/java/com/ipiecoles/java/java340/service/TechnicienServiceTest.java
+++ src/test/java/com/ipiecoles/java/java340/service/TechnicienServiceTest.java
@@ -15,39 +15,59 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import org.assertj.core.api.Assertions;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class TechnicienServiceTest {
     @InjectMocks
     public TechnicienService technicienService;
-    @Mock
-    public EmployeRepository employeRepository;
-    @Mock
-    public ManagerRepository managerRepository;
+
     @Mock
     public TechnicienRepository technicienRepository;
+
+    @Mock
+    public ManagerRepository managerRepository;
 
     public Technicien employeTechnicien;
     public Manager employeManager;
     @Test
-    public void testAddProprietaire() throws EmployeException {
-/*
+    public void testAddManager() throws EmployeException {
+
         //Given
         employeTechnicien = new Technicien();
         employeTechnicien.setNom("DURAND");
         employeTechnicien.setMatricule("C4321");
+
         employeManager = new Manager();
         employeManager.setNom("JULES");
         employeManager.setMatricule("C1234");
+
         Mockito.when(technicienRepository.findOne(Mockito.anyLong())).thenReturn(employeTechnicien);
+        Mockito.when(technicienRepository.save(Mockito.any(Technicien.class))).then(returnsFirstArg());
+
         Mockito.when(managerRepository.findByMatricule(Mockito.anyString())).thenReturn(employeManager);
-        Mockito.when(technicienRepository.save(Mockito.any(Employe.class))).then(Mockito.returnsFirstArg());
+        Mockito.when(managerRepository.save(Mockito.any(Manager.class))).then(returnsFirstArg());
+
         //When Manager addManager(Long idTechnicien, String matricule)
         technicienService.addManager(employeTechnicien.getId(), employeManager.getMatricule());
+
         //Then
-        ArgumentCaptor<Employe> vehiculeCaptor = ArgumentCaptor.forClass(Employe.class);
-        Mockito.verify(employeRepository, Mockito.times(1)).save(vehiculeCaptor.capture());
-        Assertions.assertThat(vehiculeCaptor.getValue().getProprietaireId()).isEqualTo(2L);
-        */
+        ArgumentCaptor<Technicien> technicienCaptor = ArgumentCaptor.forClass(Technicien.class);
+        ArgumentCaptor<Manager> managerCaptor = ArgumentCaptor.forClass(Manager.class);
+        Mockito.verify(technicienRepository, Mockito.times(1)).save(technicienCaptor.capture());
+        Assertions.assertThat(technicienCaptor.getValue().getManager()).isEqualTo(employeManager);
+        Mockito.verify(managerRepository, Mockito.times(1)).save(managerCaptor.capture());
+        Assertions.assertThat(managerCaptor.getValue().getEquipe()).contains(employeTechnicien);
+
     }
 }
