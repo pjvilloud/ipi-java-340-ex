18cc6ff3589f55d08ccd464c93790781717a0328
shin_taku@hotmail.com <shin_taku@hotmail.com>
2018-02-13 10:08:40 +0100

    test equivalenceNote with parameters OK

--- src/test/java/com/ipiecoles/java/java340/model/CommercialTest.java
+++ src/test/java/com/ipiecoles/java/java340/model/CommercialTest.java
@@ -3,9 +3,33 @@ package com.ipiecoles.java.java340.model;
 import com.ipiecoles.java.java340.model.Commercial;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
 public class CommercialTest {
 
+    @Parameter(value = 0)
+    public Integer performance;
+    @Parameter(value = 1)
+    public Note note;
+    @Parameters(name = "performance {0} ; note {1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {null,null},
+            {0, Note.INSUFFISANT},
+            {50, Note.INSUFFISANT},
+            {100, Note.PASSABLE},
+            {150, Note.BIEN},
+            {200, Note.TRES_BIEN},
+            {199, null}
+        });
+    }
     @Test
     public void testPerformanceNotEquals() {
         //Given
@@ -49,5 +73,15 @@ public class CommercialTest {
         Double prime = commercial.getPrimeAnnuelle();
         //Then
         Assertions.assertThat(prime).isEqualTo(500d);
+    }
+
+    @Test
+    public void testEquivalenceNote() {
+        //Given
+        Commercial commercial = new Commercial();
+        // When
+        commercial.setPerformance(performance);
+        //Then
+        Assertions.assertThat(commercial.equivalenceNote()).isEqualTo(note);
     }
 }
