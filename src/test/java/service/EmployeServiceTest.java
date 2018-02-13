e435aad46c00eb783e33386197e3ead25976cd6b
Julien Venet <venet.julien@gmail.com>
2018-02-13 12:28:33 +0100

    ajout EmployeServiceTest-fonctionnel

---
+++ src/test/java/service/EmployeServiceTest.java
@@ -0,0 +1,32 @@
+package service;
+
+import com.ipiecoles.java.java340.model.repository.EmployeRepository;
+import com.ipiecoles.java.java340.model.service.EmployeService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.persistence.EntityNotFoundException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmployeServiceTest {
+
+    @InjectMocks
+    private EmployeService employeService;
+
+    @Mock
+    private EmployeRepository employeRepository;
+
+    @Test(expected = EntityNotFoundException.class)
+    public void testFindByImmatNotFound(){
+
+        //Given
+        Mockito.when(employeRepository.findByMatricule("inconnu")).thenReturn(null);
+
+        //When
+        employeService.findByMatricule("inconnu");
+    }
+}
