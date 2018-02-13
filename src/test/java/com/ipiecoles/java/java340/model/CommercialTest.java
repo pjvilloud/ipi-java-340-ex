5706e6ae931c65891b49db84bd25f2173bfa7189
LAPTOP-97H7B5O5\manel <manel.sayadi@yahoo.fr>
2018-02-13 14:50:05 +0100

    test 3

--- src/test/java/com/ipiecoles/java/java340/model/CommercialTest.java
+++ src/test/java/com/ipiecoles/java/java340/model/CommercialTest.java
@@ -15,5 +15,47 @@ public class CommercialTest {
         //then
         Assertions.assertThat(primeAnnuelle).isEqualTo(500D);
 
+
     }
+
+    @Test
+    public void testGetPrimeAnnuelleWithCA0() {
+        //Given
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(0d);
+        //When
+        Double primeAnnuelle = commercial.getPrimeAnnuelle();
+        //then
+        Assertions.assertThat(primeAnnuelle).isEqualTo(500D);
+
+
+    }
+
+    @Test
+    public void testGetPrimeAnnuelleWithCA9000() {
+        //Given
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(9000d);
+        //When
+        Double primeAnnuelle = commercial.getPrimeAnnuelle();
+        //then
+        Assertions.assertThat(primeAnnuelle).isEqualTo(500D);
+
+
+    }
+    @Test
+    public void testGetPrimeAnnuelleWithCA100000() {
+        //Given
+        Commercial commercial = new Commercial();
+        commercial.setCaAnnuel(100000d);
+        //When
+        Double primeAnnuelle = commercial.getPrimeAnnuelle();
+        //then
+        Assertions.assertThat(primeAnnuelle).isEqualTo(5000D);
+
+    }
+
+
+
+
 }
