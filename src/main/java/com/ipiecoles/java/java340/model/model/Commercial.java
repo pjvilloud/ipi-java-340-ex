51f89f09a40a4dfc2c0cc1d079063d67aa53d61f
Julien Venet <venet.julien@gmail.com>
2018-02-14 16:39:00 +0100

    correctif-after-sonar analyse

--- src/main/java/com/ipiecoles/java/java340/model/model/Commercial.java
+++ src/main/java/com/ipiecoles/java/java340/model/model/Commercial.java
@@ -78,6 +78,7 @@ public class Commercial extends Employe {
 		return performance;
 	}
 
+	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (!(o instanceof Commercial)) return false;
