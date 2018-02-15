da7a39fd86311120e9c56b59b81e139352c011d7
Michael Chartoire <chartoire.michael@neuf.fr>
2018-02-15 10:39:00 +0100

    testtttt

--- src/test/java/com/ipiecoles/java/java340/model/ManagerTest.java
+++ src/test/java/com/ipiecoles/java/java340/model/ManagerTest.java
@@ -1,31 +1,54 @@
-//package com.ipiecoles.java.java340.model;
-//
-////Pour afficher dans le fichier "source"(dans ce cas manager.java) les cas traités par les tests : clic droit,coverage As, JUnit test.
-//import org.assertj.core.api.Assertions; //Biblio d'assertions mieux que junit donc à mettre aussi
-//import org.junit.Test;
-//
-//public class ManagerTest
-//{
-//	@Test
-//	//Les test sont tjs en class void
-//	//On test le cas où l'augmentation est nulle
-//	public void testAugmenterSalaireNull()
-//	{
-//		//Given
-//		Manager manager = new Manager();
-//		manager.setSalaire((double) 1600);
-//		
-//		//When
-//		//no suitable method found for augmenterSalaire(no arguments)
-//		 Double augmentation = manager.augmenterSalaire();
-//		// Pbl car obliger de rajouter une nouvelle methode augmenterSalaire() dans Manager.java
-//		//Then
-//		Assertions.assertThat(augmentation).isEqualTo(1600d);
-//	}
-//	
-//	@Test
-//	public void testSetSalaire() {
-//		
-//	}
-//	
-//}
+package com.ipiecoles.java.java340.model;
+
+//Pour afficher dans le fichier "source"(dans ce cas manager.java) les cas traités par les tests : clic droit,coverage As, JUnit test.
+import org.assertj.core.api.Assertions; //Biblio d'assertions mieux que junit donc à mettre aussi
+import org.junit.Test;
+
+public class ManagerTest
+{
+	
+	@Test
+	//Les test sont tjs en class void
+	public void testSetSalaire()
+	{
+		//TEST : voir si le salaire en sortie est sup au salaire en entrée
+		//
+		//Given
+		Manager manager = new Manager();
+		manager.setSalaire(1600d);
+		
+		//When
+		//Double salaireEntree = manager.getSalaire();
+		Double salaireSortie = manager.getSalaire();
+		//Then
+		Assertions.assertThat(salaireSortie).isGreaterThan(manager.getSalaire());
+	}
+	
+	/*public void setSalaire(Double salaire) {
+		super.setSalaire(salaire * Entreprise.INDICE_MANAGER + (salaire * (double)equipe.size() / 10));
+	}*/
+	
+	
+	/*@Test
+	//Les test sont tjs en class void
+	//On test le cas où l'augmentation est nulle
+	public void testAugmenterSalaireNull()
+	{
+		//Given
+		Manager manager = new Manager();
+		manager.setSalaire((double) 1600);
+		
+		//When
+		//no suitable method found for augmenterSalaire(no arguments)
+		 Double augmentation = manager.augmenterSalaire();
+		// Pbl car obliger de rajouter une nouvelle methode augmenterSalaire() dans Manager.java
+		//Then
+		Assertions.assertThat(augmentation).isEqualTo(1600d);
+	}
+	
+	@Test
+	public void testSetSalaire() {
+		
+	}*/
+	
+}
--- src/test/java/com/ipiecoles/java/java340/service/TechnicienServiceTest.java
+++ src/test/java/com/ipiecoles/java/java340/service/TechnicienServiceTest.java
@@ -34,14 +34,17 @@ public class TechnicienServiceTest
 	public void testAddManager()
 	{
 		//Given
-		final String MATRICULE = "M12345";
+		//ON declare 2 constantes car pour des tests il est preferable d'avoir des valeurs fixes. 
+		final String MATRICULE = "M12345"; //Matricule existant dans la BDD
 		final Long ID_TECHNICIEN = 1L;
+		//On initialise 2 employés : un technicien et un manager
 		Technicien technicien = new Technicien("Dure", "jacques", "C12353", new LocalDate(), 1600d, 3);
 		Manager manager = new Manager("Rond", "jo", MATRICULE, new LocalDate(), 1600d, new HashSet<>());
 		//On passe par les Mocks pour renvoyer les valeurs qui se subsistent au managerRepository et TechnicienRepository
+		//On dit donc au Mock de substituer les 2 variables créees aux manager et technicien issus du Repository
 		Mockito.when(technicienRepository.findOne(ID_TECHNICIEN)).thenReturn(technicien);
 		Mockito.when(managerRepository.findByMatricule(MATRICULE)).thenReturn(manager);
-		//Dans ce cas la ligne suivante dit 
+		//Dans ce cas la ligne suivante dit quand le manager est enregister dans la bdd, tu me renvoies la valeur
 		Mockito.when(managerRepository.save(Mockito.any(Manager.class))).then(AdditionalAnswers.returnsFirstArg());
 		Mockito.when(technicienRepository.save(Mockito.any(Technicien.class))).then(AdditionalAnswers.returnsFirstArg());
 		//When
@@ -50,8 +53,9 @@ public class TechnicienServiceTest
 		//le .times() est un .count().Comme on a un seul technicien, on s'assure qu'on est passé qu'une seule fois
 		Mockito.verify(technicienRepository, Mockito.times(1)).findOne(ID_TECHNICIEN);
 		Mockito.verify(managerRepository, Mockito.times(1)).findByMatricule(MATRICULE);
-		
+		// On s'assure que l'on ne passe qu'un manager dans l'équipe ?
 		Assertions.assertThat(manager.getEquipe()).hasSize(1);
+		//ON s'assure qu'il y a bien un technicien dans l'équipe ?
 		Assertions.assertThat(manager.getEquipe()).contains(technicien);
 		
 		Assertions.assertThat(technicien.getManager()).isEqualTo(manager);
