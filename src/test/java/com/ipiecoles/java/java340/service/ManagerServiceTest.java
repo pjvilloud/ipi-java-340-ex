7a7b9d55a7a36fc46c9953ce02e82a11c2c36390
Mata Reia <matatini.reia@live.fr>
2018-02-25 15:31:44 +0100

    initialized ManagerServiceTest.java

--- src/test/java/com/ipiecoles/java/java340/service/ManagerServiceTest.java
+++ src/test/java/com/ipiecoles/java/java340/service/ManagerServiceTest.java
@@ -1,4 +1,33 @@
 package com.ipiecoles.java.java340.service;
 
+import com.ipiecoles.java.java340.exception.EmployeException;
+import com.ipiecoles.java.java340.repository.ManagerRepository;
+import com.ipiecoles.java.java340.repository.TechnicienRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ManagerServiceTest {
+
+    @InjectMocks
+    ManagerService managerService;
+
+    @Mock
+    ManagerRepository managerRepository;
+    @Mock
+    TechnicienRepository technicienRepository;
+
+    @Before
+    public void setUp() throws EmployeException {
+
+    }
+    
+    @Test
+    public void testAddTechnicien() {
+
+    }
 }
