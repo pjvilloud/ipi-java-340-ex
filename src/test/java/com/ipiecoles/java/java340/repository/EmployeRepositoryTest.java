b895500df772523ebd38246f8df864d51fd58f80
dylanben <dylan.bengueddache@cgi.com>
2018-03-01 15:28:08 +0100

    test travis

--- src/test/java/com/ipiecoles/java/java340/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java340/repository/EmployeRepositoryTest.java
@@ -10,15 +10,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.ipiecoles.java.java340.SpringWebApplication;
 import com.ipiecoles.java.java340.exception.EmployeException;
 import com.ipiecoles.java.java340.model.Commercial;
 import com.ipiecoles.java.java340.model.Employe;
 import com.ipiecoles.java.java340.model.Technicien;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+//@DataJpaTest
+@SpringBootTest(classes = SpringWebApplication.class)
 public class EmployeRepositoryTest {
 
 	@Autowired
