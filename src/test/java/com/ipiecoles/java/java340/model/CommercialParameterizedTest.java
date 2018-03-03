1648a97f32c4fc9a377d7c4f28908cc6b51b7a4f
rousslud <rousselot.ludovic@yahoo.fr>
2018-03-03 18:14:28 +0100

    Parameterized Tests: EquivalenceNote

--- src/test/java/com/ipiecoles/java/java340/model/CommercialParameterizedTest.java
+++ src/test/java/com/ipiecoles/java/java340/model/CommercialParameterizedTest.java
@@ -11,12 +11,15 @@ import java.util.Collection;
 @RunWith(value = Parameterized.class)
 public class CommercialParameterizedTest {
 
+	// La performance est le parametre 0
     @Parameterized.Parameter(value = 0)
     public Integer perf;
 
+    // La note attendue est le parametre 1
     @Parameterized.Parameter(value = 1)
     public Note expectedNote;
 
+    // On crée une liste de valeurs attendues : à gauche la performance, à droite la note attendue 
     @Parameterized.Parameters(name = "performance {0} équivalent à {1}")
     public static Collection<Object[]> data(){
         return Arrays.asList(new Object[][]{
@@ -26,10 +29,12 @@ public class CommercialParameterizedTest {
                 {150, Note.BIEN},
                 {200, Note.TRES_BIEN},
                 {null, null},
-                {600, null}
+                {600, null},
+                {151, null}
         });
     }
 
+    // Méthode permettant de lancer le test : on définit la valeur de la performance en utilisant le paramètre 0 (on teste donc 8 valeurs en tout). On y applique alors la fonction de model commercial pour définir la note équivalente. Finalement on teste si la note attendue (donc le parametre 1) correspond bien à ce qui a été fait par la méthode equivalenceNote(); 
     @Test
     public void testEquivalenceNote(){
         //Given
@@ -42,4 +47,5 @@ public class CommercialParameterizedTest {
         //Then
         Assertions.assertThat(note).isEqualTo(expectedNote);
     }
+    
 }
