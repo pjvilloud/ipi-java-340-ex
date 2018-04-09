467a85a1e28321ad12d0e83806533a16046490cf
CASTRO, Florian <florian.castro@cgi.com>
2018-04-09 13:02:25 +0200

    First empty test attempt

---
+++ src/test/java/com/ipiecoles/java/java340/model/ManagerTest.java
@@ -0,0 +1,12 @@
+package com.ipiecoles.java.java340.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class ManagerTest {
+	
+	@Test public void emptyTest() {
+		Assertions.assertThat(1).isEqualTo(1);
+	}
+	
+}
