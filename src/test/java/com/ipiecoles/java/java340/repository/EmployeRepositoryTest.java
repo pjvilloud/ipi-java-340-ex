a06deee254e62ce8e2a7a8f9e527e6216467f4e0
CASTRO, Florian <florian.castro@cgi.com>
2018-04-17 05:02:15 +0200

    Commit test Travis 9

--- src/test/java/com/ipiecoles/java/java340/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java340/repository/EmployeRepositoryTest.java
@@ -1,10 +1,9 @@
 package com.ipiecoles.java.java340.repository;
 
-import com.ipiecoles.java.java340.model.Commercial;
-import com.ipiecoles.java.java340.model.Employe;
-import org.assertj.core.api.Assertions;
 import org.joda.time.LocalDate;
-import org.junit.After;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,65 +12,67 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import com.ipiecoles.java.java340.SpringWebApplication;
+import com.ipiecoles.java.java340.model.Commercial;
+import com.ipiecoles.java.java340.model.Employe;
+import com.ipiecoles.java.java340.repository.CommercialRepository;
+import com.ipiecoles.java.java340.repository.EmployeRepository;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest(classes = SpringWebApplication.class)
 public class EmployeRepositoryTest {
+	
+	@Autowired
+	EmployeRepository employeRepository;
+	
+	// Déclaration des attributs 
+	Commercial commercial, commercial1, commercial2;
+	
+	@Before
+	public void setUp() {
+		employeRepository.deleteAll(); 
+		commercial = new Commercial("Jeanbouin", "Luc", "C12346", new LocalDate(), 1500d, 0d, 0);
+		commercial1 = new Commercial("Jeanton", "Louis", "C12347", new LocalDate(), 1200d, 0d, 0);
+		commercial2 = new Commercial("Hourran", "Alain", "C12348", new LocalDate(), 1300d, 0d, 0);
+		// Sauvegarde dans la base de données
+		commercial = employeRepository.save(commercial);
+		commercial1 = employeRepository.save(commercial1);
+		commercial2 = employeRepository.save(commercial2);
+	}
+	
+	
+	/*@Test
+	public void findByNomOrPrenomAllIgnoreCaseNom() {
+		
+		List<Employe> employes = employeRepository.findByNomOrPrenomAllIgnoreCase("jeanbouin");
+		Assertions.assertThat(employes).hasSize(1); 
+		Assertions.assertThat(employes).contains(commercial); 
+	}
+	
+	@Test
+	public void findByNomOrPrenomAllIgnoreCaseNomPrenom() {
+		
+		List<Employe> employes = employeRepository.findByNomOrPrenomAllIgnoreCase("louis");
+		Assertions.assertThat(employes).hasSize(1); 
+		Assertions.assertThat(employes).contains(commercial1); 
+	}
+	
+	@Test
+	public void findByNomOrPrenomAllIgnoreCaseNotFound() {
+		
+		List<Employe> employes = employeRepository.findByNomOrPrenomAllIgnoreCase("toto");
+		Assertions.assertThat(employes).isEmpty();
+	}*/
+	
+	@Test
+	public void findEmployesPlusRichesHasSize() {
+		List<Employe> employes = employeRepository.findEmployePlusRiches();
+		Assertions.assertThat(employes).hasSize(1); 
+	}
+	
+	public void findEmployesPlusRichesContains() {
+		List<Employe> employes = employeRepository.findEmployePlusRiches();
+		Assertions.assertThat(employes).contains(commercial); 
+	}
 
-    Commercial pierreDurand = new Commercial("Durand", "Pierre", "C12345", new LocalDate(), 1500d, 0d,0);
-
-    Commercial jeanJacques = new Commercial("Jacques", "Jean", "C12346", new LocalDate(), 1500d, 0d,0);
-
-    Commercial jacquesDupond = new Commercial("Dupond", "Jacques", "C12347", new LocalDate(), 1500d, 0d,0);
-
-    @Before
-    public void setUp(){
-        commercialRepository.deleteAll();
-        employeRepository.deleteAll();
-        employeRepository.save(pierreDurand);
-        employeRepository.save(jeanJacques);
-        employeRepository.save(jacquesDupond);
-    }
-
-    @Autowired
-    EmployeRepository employeRepository;
-
-    @Autowired
-    CommercialRepository commercialRepository;
-
-    @Test
-    public void testFindByNomOrPrenomAllIgnoreCasePrenom(){
-        //Given
-
-        //When
-        List<Employe> employes = employeRepository.findByNomOrPrenomAllIgnoreCase("pierre");
-        Assertions.assertThat(employes).hasSize(1);
-        Assertions.assertThat(employes.get(0)).isEqualTo(pierreDurand);
-    }
-
-    @Test
-    public void testFindByNomOrPrenomAllIgnoreCaseNom(){
-        //Given
-
-        //When
-        List<Employe> employes = employeRepository.findByNomOrPrenomAllIgnoreCase("durand");
-        Assertions.assertThat(employes).hasSize(1);
-        Assertions.assertThat(employes.get(0)).isEqualTo(pierreDurand);
-    }
-
-    @Test
-    public void testFindByNomOrPrenomAllIgnoreCaseNomPrenom(){
-        //Given
-
-        //When
-        List<Employe> employes = employeRepository.findByNomOrPrenomAllIgnoreCase("jacques");
-        Assertions.assertThat(employes).hasSize(2);
-        Assertions.assertThat(employes).contains(jeanJacques, jacquesDupond);
-    }
-
-    @After
-    public void tearDown(){
-        employeRepository.deleteAll();
-    }
 }
